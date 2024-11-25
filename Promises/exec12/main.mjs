fe833526f2ab416f1bd967d919f6b89b511103f8
Matheus Souza <a.matheussouza@gmail.com>
2024-11-25 11:08:04 -0300

    capitulo 7 - async await exec 12

--- Promises/exec12/main.mjs
+++ Promises/exec12/main.mjs
@@ -1,32 +1,18 @@
-// Exercício 11
+// Exercício 12
 
 // Objetivo
-// Crie um atraso no retorno da função execute por meio de setTimeout e utilize uma promise para tratar o comportamento assíncrono.
+
+// Utilize async/await para tratar as chamadas para as promises.
 
 // Instruções
-// Envolva o código da função execute em um setTimeout com 1000ms.
-// Crie uma promise e retorne-a.
-// Execute o comando "create table".
-// Após a execução de "create table", utilize a função Promise.all para executar os 3 comandos "insert".
-// Após a execução dos 3 comandos "insert", faça um select retornando as colunas "name" e "author".
-
-// Resultado
-// [{
-//     "name": "Douglas Crockford",
-//     "age": "62"
-// }, {
-//     "name": "Linus Torvalds",
-//     "age": "47"
-// }, {
-//     "name": "Martin Fowler",
-//     "age": "54"
-// }]
-
+// Crie uma função utilizando async.
+// Invoque cada uma das funções execute utilizando await incluindo o Promise.all.
+// Envolva as chamadas em um bloco try/catch para tratar as exceções.
 
 // Conteúdo abordado neste exercício
-// Promise
-// Promise.all
-// new
+// Async/Await
+// Promises
+// try/catch
 
 import Database from './database.mjs'
 
